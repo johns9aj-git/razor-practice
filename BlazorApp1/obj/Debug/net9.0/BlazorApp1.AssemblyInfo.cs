31c1ef128867e80afb452b6f5b0f6dac923f968f
ajohns2020 <johns9aj@mail.uc.edu>
2026-02-22 10:56:26 -0500

    Added Todo Page

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32c7067e665952dfa425715eb0fbdfb9cc537f0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d62aa3a4c1882ac1c1dfb0457406b2e838c9404e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
